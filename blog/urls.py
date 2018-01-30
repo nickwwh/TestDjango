5702344aa13f155bc2da489afb3ea5eca8d8db80
Nick <nicholaswong@smu.edu.sg>
2018-01-30 18:19:45 +0800

    lesson 6&7

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+"""
+"""
+
+from django.urls import path
+from django.views.generic import ListView
+from blog.models import Post
+
+urlpatterns = [
+    path('', ListView.as_view(
+        queryset=Post.objects.all().order_by("-date")[:25],
+        template_name="blog/blog.html")),
+]
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('webapp/', include('webapp.urls')),
     path('', include('personal.urls')),
+    path('blog/', include('blog.urls'))
 ]
